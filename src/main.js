5b473489c594897c7bcd5ea2a0d785318a7d41e3
smate314 <34484840+smate314@users.noreply.github.com>
2025-08-22 11:08:07 +0100

    Fill in personal details

--- src/main.js
+++ src/main.js
@@ -4,15 +4,15 @@ new App({
   target: document.body,
   props: {
     // What's your name?
-    name: "James Butler",
+    name: "Máté Soos",
     // In the following fiels you can either give a single string,
     // or an array of bullet points
 
     // What do you associate with the term 'CI/CD'?
-    associations: ["Ehm, what?"],
+    associations: ["testing","deployment","integration","automation"],
     // Which CI/CD tools do you use in your project?
-    tools: "Jenkins, maybe?",
+    tools: "Jenkins, GitHub actions",
     // What do you want to learn in this workshop?
-    expectations: ["what you are talking about"],
+    expectations: ["Fill in the gaps in my current basic ci/cd knowledge"],
   },
 });
